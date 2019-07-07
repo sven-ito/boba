2d42c37facd8144bcdb1b1d68d4c67e5ce9f2f83
Sven Scheuring <sven.scheuring@hotmail.com>
2019-07-07 11:40:57 +0200

    refactoring part 1

--- TODos.md
+++ TODos.md
@@ -1,19 +1,18 @@
 TODO:
 
-Helpbox with advice on how to make a decision (left side, Bootstrap Affix?) : 7
-code refactoring : 6
-Data persistence (DOM Storage / Cookie / Client side) : 4
-versioning : 4
-decision boxes with comment fields on why the decision was taken : 4
-undo buttons : 2
-separate decision boxes with counter (e.g. "7/10") : 1
+code refactoring : 11
+versioning : 10
+Helpbox with advice on how to make a decision (left side, Bootstrap Affix?) : 8
+decision boxes with comment fields on why the decision was taken : 8
+grey out buttons that can no yet be used : 8
+create Boba (Fett) favicon : 7
+undo buttons : 6
+reduce number of buttons : 6
+separate decision boxes with counter (e.g. "7/10") : 5
+text hint boxes should be hidden and only appear once the step can be done : 5
+Data persistence (DOM Storage / Cookie / Client side) : 2
+progress bar : 2
 server side coding : 0
-
-reduce number of buttons
-grey out buttons that can no yet be used
-text hint boxes should be hidden and only appear once the step can be done
-create Boba (Fett) favicon
-progress bar
 
 DONE:
 
--- main.js
+++ main.js
@@ -1,47 +1,51 @@
-var form = document.getElementById('addForm');
-var form2 = document.getElementById('collectForm');
-var itemList = document.getElementById('items');
-var choicesList = document.getElementById('choicesList');
-var evaluationList = document.getElementById('evaluationList');
+// ------------------------------------------------------------------------------
 
-var textArea = document.getElementById('prioItems');
+// All relevant GUI components
+const TEXT_AREA = document.getElementById('prioItems');
+const ITEM_LIST_FORM = document.getElementById('addForm');
+const CHOICES_FORM = document.getElementById('collectForm');
+const ITEM_LIST = document.getElementById('items');
+const CHOICES_LIST = document.getElementById('choicesList');
+const EVAL_LIST = document.getElementById('evaluationList');
+const EVAL_BUTTON = document.getElementById('evaluationButton');
 
-var evaluationButton = document.getElementById('evaluationButton');
-
+// All data structures for (processed) inputs - as global variables
 var items = [];
 var choices = [];
 var scoring = [];
 
-// Form submit event
-form.addEventListener('submit', addItems);
-form2.addEventListener('submit', createChoices);
-evaluationButton.addEventListener('click', evaluatePrios);
+// Add event handlers
+ITEM_LIST_FORM.addEventListener('submit', addItems);
+CHOICES_FORM.addEventListener('submit', createChoices);
+EVAL_BUTTON.addEventListener('click', evaluatePrios);
 
+
+// ------------------------------------------------------------------------------
 
 //Add item
 function addItem(newItem) {
 
-  // Get input value
-  //var newItem = document.getElementById('item').value;
+    // Get input value
+    //var newItem = document.getElementById('item').value;
 
-  // Create new li element
-  var li = document.createElement('li');
-  // Add class
-  li.className = 'list-group-item';
-  // Add text node with input value
-  li.appendChild(document.createTextNode(newItem));
+    // Create new li element
+    let li = document.createElement('li');
+    // Add class
+    li.className = 'list-group-item';
+    // Add text node with input value
+    li.appendChild(document.createTextNode(newItem));
 
-  // Append li to list
-  itemList.appendChild(li);
+    // Append li to list
+    ITEM_LIST.appendChild(li);
 }
 
 function addItems(e) {
 
     e.preventDefault();
 
-    var lines = textArea.value.split("\n");
+    let lines = TEXT_AREA.value.split("\n");
 
-    for (var i = 0; i < lines.length; i++) {
+    for (let i = 0; i < lines.length; i++) {
         addItem(lines[i]);
     }
 
@@ -52,97 +56,97 @@ function createChoices(e) {
     e.preventDefault();
 
     // Collect list items into an array / list
-    for (var i = 0; i < itemList.children.length; i++) {
-        items[i] = itemList.children[i].innerText;
+    for (let i = 0; i < ITEM_LIST.children.length; i++) {
+        items[i] = ITEM_LIST.children[i].innerText;
     }
     console.log(items);
 
-    for (var i = 0; i < items.length; i++) {
+    for (let i = 0; i < items.length; i++) {
         scoring[i] = 0;
     }
     console.log(scoring);
 
     //Create combinations
-    for (var i = 0; i < items.length-1; i++) {
+    for (let i = 0; i < items.length - 1; i++) {
 
         console.log(items[i]);
 
-        var currentSubList = items.slice(i+1,items.length)
-        
+        let currentSubList = items.slice(i + 1, items.length)
+
         console.log(currentSubList);
-        for (var j = 0; j < currentSubList.length; j++) {
-            
+        for (let j = 0; j < currentSubList.length; j++) {
+
             //console.log([items[i], currentSubList[j]]);
             //choices.push([items[i], currentSubList[j]]);
-            
-            console.log([i, i+1+j]);
-            choices.push([i, i+1+j]);
+
+            console.log([i, i + 1 + j]);
+            choices.push([i, i + 1 + j]);
         }
     }
 
     console.log(choices);
 
-    for (var i = 0; i < choices.length; i++) {
+    for (let i = 0; i < choices.length; i++) {
 
         // Create new li element
-        var li = document.createElement('li');
+        let li = document.createElement('li');
         li.className = 'list-group-item';
 
-        var div = document.createElement('div');
+        let div = document.createElement('div');
         div.className = 'row';
-       
-        var indexOptionA = choices[i][0];
+
+        let indexOptionA = choices[i][0];
         console.log(choices[i][0]);
 
-        var div2 = document.createElement('div');
+        let div2 = document.createElement('div');
         div2.className = 'col-sm';
-        var button2 = document.createElement('button');
+        let button2 = document.createElement('button');
         button2.className = 'btn btn-primary';
         button2.appendChild(document.createTextNode(items[indexOptionA]));
         button2.dataset.indexOption = indexOptionA;
         button2.addEventListener(
-            'click', 
-            function() {
-                console.log("Option A: "+this.dataset.indexOption);
+            'click',
+            function () {
+                console.log("Option A: " + this.dataset.indexOption);
                 scoring[this.dataset.indexOption]++;
 
-                var allButtons = this.parentElement.parentElement.getElementsByClassName('btn');
-                for (var i = 0; i < allButtons.length; i++) {
+                let allButtons = this.parentElement.parentElement.getElementsByClassName('btn');
+                for (let i = 0; i < allButtons.length; i++) {
                     allButtons[i].disabled = true;
                     allButtons[i].className = 'btn btn-dark';
                 }
 
                 this.className = 'btn btn-success';
-        },
-        false
+            },
+            false
         );
         div2.appendChild(button2);
         div.appendChild(div2);
 
-        var indexOptionB = choices[i][1];
+        let indexOptionB = choices[i][1];
         console.log(choices[i][1]);
 
-        var div3 = document.createElement('div');
+        let div3 = document.createElement('div');
         div3.className = 'col-sm';
-        var button3 = document.createElement('button');
+        let button3 = document.createElement('button');
         button3.className = 'btn btn-info';
         button3.appendChild(document.createTextNode(items[indexOptionB]));
         button3.dataset.indexOption = indexOptionB;
         button3.addEventListener(
-            'click', 
-            function() {
-                console.log("Option B: "+this.dataset.indexOption);
+            'click',
+            function () {
+                console.log("Option B: " + this.dataset.indexOption);
                 scoring[this.dataset.indexOption]++;
 
-                var allButtons = this.parentElement.parentElement.getElementsByClassName('btn');
-                for (var i = 0; i < allButtons.length; i++) {
+                let allButtons = this.parentElement.parentElement.getElementsByClassName('btn');
+                for (let i = 0; i < allButtons.length; i++) {
                     allButtons[i].disabled = true;
                     allButtons[i].className = 'btn btn-dark';
                 }
 
                 this.className = 'btn btn-success';
-        },
-        false
+            },
+            false
         );
         div3.appendChild(button3);
         div.appendChild(div3);
@@ -151,7 +155,7 @@ function createChoices(e) {
 
         //li.appendChild(document.createTextNode(items[choices[i][0]]+" vs "+items[choices[i][1]]));
         // Append li to list
-        choicesList.appendChild(li);
+        CHOICES_LIST.appendChild(li);
 
     }
 }
@@ -159,38 +163,38 @@ function createChoices(e) {
 // from https://stackoverflow.com/questions/3730510/javascript-sort-array-and-return-an-array-of-indicies-that-indicates-the-positi
 
 function sortWithIndeces(toSort) {
-    for (var i = 0; i < toSort.length; i++) {
-      toSort[i] = [toSort[i], i];
+    for (let i = 0; i < toSort.length; i++) {
+        toSort[i] = [toSort[i], i];
     }
-    toSort.sort(function(left, right) {
+    toSort.sort(function (left, right) {
         // Modified: '<' --> '>' for reverse order
-      return left[0] > right[0] ? -1 : 1;
+        return left[0] > right[0] ? -1 : 1;
     });
     toSort.sortIndices = [];
-    for (var j = 0; j < toSort.length; j++) {
-      toSort.sortIndices.push(toSort[j][1]);
-      toSort[j] = toSort[j][0];
+    for (let j = 0; j < toSort.length; j++) {
+        toSort.sortIndices.push(toSort[j][1]);
+        toSort[j] = toSort[j][0];
     }
     return toSort;
-  }
+}
 
 function evaluatePrios(e) {
 
     console.log(scoring);
     sortWithIndeces(scoring);
 
-    for (var i = 0; i < scoring.length; i++) {
+    for (let i = 0; i < scoring.length; i++) {
 
         // Create new li element
-        var li = document.createElement('li');
+        let li = document.createElement('li');
         li.className = 'list-group-item';
 
-        li.appendChild(document.createTextNode(items[scoring.sortIndices[i]]+" : "+scoring[i]));
-        evaluationList.appendChild(li);
+        li.appendChild(document.createTextNode(items[scoring.sortIndices[i]] + " : " + scoring[i]));
+        EVAL_LIST.appendChild(li);
 
     }
 
-    
+
 }
 
 
